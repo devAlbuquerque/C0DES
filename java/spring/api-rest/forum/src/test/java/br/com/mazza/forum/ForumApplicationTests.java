057e5b17f7d5fd060fce9ddad6edd7cd29ecdea0
Antonio E. A. de Freitas <antonio.erismar@gmail.com>
2021-02-25 00:05:59 -0300

    Disponibilizando projeto de Api Rest

---
+++ java/spring/api-rest/forum/src/test/java/br/com/mazza/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mazza.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
